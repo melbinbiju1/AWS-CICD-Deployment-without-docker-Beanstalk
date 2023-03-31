4c1d4534c44a001e30b1d67b5588f060edc59aef
melbin biju <melbinbiju212330@gmail.com>
2023-03-31 13:12:04 +0530

    port host changed

--- application.py
+++ application.py
@@ -29,5 +29,5 @@ def predictPrice():
 
 
 if __name__ == '__main__':
-    # app.run(host='127.0.0.1',port=8080,debug=True)   # for local run
-    app.run(host='0.0.0.0',port=8080)  # for deployment run
+    # app.run(host='0.0.0.0',port=8080,debug=True)   # for local run
+    app.run(host='127.0.0.1',port=8000)  # for deployment run
