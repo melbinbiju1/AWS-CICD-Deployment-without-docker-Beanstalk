7ad694d32b293f0161adfa31ddf06c287ff4d426
melbin biju <melbinbiju212330@gmail.com>
2023-03-31 16:26:04 +0530

    app run modified

--- application.py
+++ application.py
@@ -30,5 +30,5 @@ def predictPrice():
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0',port=8080,debug=True)   # for local run
-    # app.run(host='127.0.0.1',port=8000)  # for deployment run
-    app.run(host="0.0.0.0")        
+    app.run(host='0.0.0.0',port=8080)  # for deployment run
+        
